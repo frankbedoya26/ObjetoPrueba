988a5e04ea0c96ac68d0fe0a0454ca4fe98055b0
frankbedoya26 <frank222bedoya@gmail.com>
2022-10-02 17:21:32 -0500

    Se agrega el proyecto

---
+++ src/objetoprueba/ObjetoPrueba.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetoprueba;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ObjetoPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        String nom;
+        nom=JOptionPane.showInputDialog("Ingrese su nombre");
+        JOptionPane.showMessageDialog(null, "Su nombre es "+nom);
+    }
+    
+}
